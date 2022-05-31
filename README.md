dc27c2eb79db83196286eec1abb69b476e616d24
Kirill Repkin <kirill@repkin.su>
2022-05-31 14:36:12 +0300

    packer conf

---
+++ README.md
@@ -0,0 +1,12 @@
+```bash
+cp backend.conf.default backend.conf
+cp variables.env.default variables.env
+```
+
+```bash
+export $(cat variables.env | xargs)
+```
+
+```bash
+packer build  -var token=$TF_VAR_token -var folder_id=$TF_VAR_folder_id -var subnet_id=$TF_VAR_subnet_id packer.json
+```
--- provision.sh
+++ provision.sh
@@ -21,4 +21,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
